10664cbf7dc8ad7c6eae4be64b6a0ed89f22ca44
Ubuntu <kmitt006@ucr.edu>
2021-10-13 01:48:18 -0700

    Plz halp

--- source/main.c
+++ source/main.c
@@ -8,22 +8,16 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+
 volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 0;
+unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
 
 
-int lights[] = {0x01, 0x02, 0x04, 0x02};
-enum States{init, Light, stop} lightStates;
-unsigned int i = 0;
-unsigned char tmpA = 0x00;
-unsigned char tmpB = 0x00;
-unsigned char counter = 1;
 
 void TimerOn(){
 	TCCR1B = 0x0B;
@@ -54,37 +48,127 @@ void TimerSet(unsigned long M){
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-void tick(){
-	switch(lightStates){
-		case init:
-			lightStates = Light;
-			i = 0;
-			break;
-		case Light:
-			tmpB = lights[i % 4];
-			i+=counter;
-			lightStates = Light;
-			if(tmpA & 0x01)
-				lightStates = stop;
-			break;
-		case stop:
-			lightStates = stop;
-			if(tmpA & 0x01)
-				lightStates = Light;
-			break;
-	}
-}
+unsigned char tmpA = 0x00;
+unsigned char tmpB = 0x00;
+unsigned char periodCounter = 0;
 
+enum States{start, wait, increment, decrement, reset} counterState;
+
+void tick(){
+  switch(counterState){
+    case start:
+      tmpB = 0x07;
+      counterState = wait;
+	  periodCounter = 0;
+      break;
+	  
+    case wait:
+		periodCounter = 0;
+		tmpB = 0xFF;
+      if((tmpA & 0x01) && !(tmpA & 0x02)){
+        counterState = increment; 
+        if(tmpB < 9)
+          tmpB++;
+      }
+      else if(!(tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = decrement; 
+        if(tmpB > 0)
+          tmpB--;
+      }
+      else if((tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = reset; 
+        tmpB = 0;
+      }
+      else if(!(tmpA & 0x01) && !(tmpA & 0x02))
+        counterState = wait;
+
+      break;
+	  
+    case increment:
+      if((tmpA & 0x01) && !(tmpA & 0x02)){
+        counterState = increment;
+		if(periodCounter <= 10)
+			periodCounter++;
+		else if(periodCounter > 10){
+			if(tmpB < 9){
+				tmpB++;
+				periodCounter = 0;
+			}
+		}
+      }
+      else if(!(tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = decrement;
+		periodCounter = 0;
+        if(tmpB > 0)
+          tmpB--;
+      }
+      else if((tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = reset; 
+        tmpB = 0;
+      }
+      else if(!(tmpA & 0x01) && !(tmpA & 0x02))
+        counterState = wait;
+      break;
+
+    case decrement:
+      if((tmpA & 0x01) && !(tmpA & 0x02)){
+		  periodCounter = 0;
+        counterState = increment;
+        if(tmpB < 9)
+          tmpB++;
+      }
+      else if(!(tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = decrement;
+		if(periodCounter <= 10)
+			periodCounter++;
+		if(periodCounter > 10){
+			if(tmpB > 0){
+				tmpB--;
+				periodCounter = 0;
+			}
+		}
+      }
+      else if((tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = reset; 
+        tmpB = 0;
+      }
+      else if(!(tmpA & 0x01) && !(tmpA & 0x02))
+        counterState = wait;
+      break;
+
+    case reset:
+		periodCounter = 0;
+      if((tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = reset; 
+        tmpB = 0;
+      }
+      else if(!(tmpA & 0x01) && !(tmpA & 0x02))
+        counterState = wait;       
+      else if((tmpA & 0x01) && !(tmpA & 0x02)){
+        counterState = increment;
+        if(tmpB < 9)
+          tmpB++;
+      }
+      else if(!(tmpA & 0x01) && (tmpA & 0x02)){
+        counterState = increment;
+        if(tmpB > 0)
+          tmpB--;
+      }
+      break;
+  } 
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRB  = 0xFF;
-	PORTB = 0x00;
-	TimerSet(300);
-	TimerOn();
+	DDRA = 0x00; tmpA = 0x00; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; tmpB = 0x00;
     /* Insert your solution below */
+	TimerSet(100);
+	TimerOn();
+	
+	counterState = start;
     while (1) {
-		PORTA = PINA;
-		tmpA = PORTA;
+		PORTA = ~PINA;
+		tmpA = PORTA & 0x03;
 		tick();
 		PORTB = tmpB;
 		while(!TimerFlag);
